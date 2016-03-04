21067fe5ace7ba9421ab1fd280ba181b719b8bcf
Vaughn Dice <vdice@deis.com>
2016-03-04 16:26:35 -0700

    fix(version_test): version now includes 7-character git sha
    Fixes #80

--- tests/version_test.go
+++ tests/version_test.go
@@ -6,11 +6,12 @@ import (
 )
 
 var _ = Describe("Version", func() {
+	ourSemVerRegExp := `^v?(?:[0-9]+\.){2}[0-9]+`
+	gitSHARegExp := `[0-9a-f]{7}`
 
 	It("prints its version", func() {
 		output, err := execute("deis --version")
 		Expect(err).NotTo(HaveOccurred())
-		// TODO: read the expected version from ../client/deis-version?
-		Expect(output).To(Equal("2.0.0-dev\n"))
+		Expect(output).To(MatchRegexp(`%s(-dev)?(-%s)?\n`, ourSemVerRegExp, gitSHARegExp))
 	})
 })
