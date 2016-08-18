30c8fd343c480b266542ab602cebe04401f4030d
smothiki <smothiki@engineyard.com>
2016-08-18 16:41:35 -0600

    feat(procfile): unskip builds procfile test

--- tests/buildprocfile_test.go
+++ tests/buildprocfile_test.go
@@ -1,8 +1,6 @@
 package tests
 
 import (
-	"time"
-
 	"github.com/deis/workflow-e2e/tests/cmd"
 	"github.com/deis/workflow-e2e/tests/cmd/apps"
 	"github.com/deis/workflow-e2e/tests/cmd/auth"
@@ -12,6 +10,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("deis builds procfile", func() {
@@ -39,29 +38,27 @@ var _ = Describe("deis builds procfile", func() {
 			AfterEach(func() {
 				apps.Destroy(user, app)
 			})
-			// https://github.com/deis/workflow-e2e/issues/299
-			// TODO(smothiki): somehow determine why this test is flaky in jenkins after merging deployments
-			XSpecify("that user create a new build of that app with a different procfile", func() {
-				// Docker Hub gives a "not found" 400 error
+			Specify("that user can create a new build of that app with a different procfile", func() {
 				Image := "smothiki/exampleapp:latest"
 				procfile := "web: /bin/boot"
 				sess, err := cmd.Start("deis pull %s --app=%s ", &user, Image, app.Name)
 				Expect(err).NotTo(HaveOccurred())
 				Eventually(sess).Should(Say("Creating build..."))
-				time.Sleep(30000 * time.Millisecond)
+				Eventually(sess, settings.MaxEventuallyTimeout).Should(Exit(0))
 				sess, err = cmd.Start("deis builds:create %s -a %s --procfile \"%s\"", &user, Image, app.Name, procfile)
 				Expect(err).NotTo(HaveOccurred())
 				Eventually(sess).Should(Say("Creating build..."))
-				time.Sleep(20000 * time.Millisecond)
+				Eventually(sess, settings.MaxEventuallyTimeout).Should(Exit(0))
 				sess, err = cmd.Start("deis ps:scale web=1 -a %s", &user, app.Name)
 				Expect(err).NotTo(HaveOccurred())
-				Eventually(sess).Should(Say("Scaling processes... but first,"))
-				time.Sleep(20000 * time.Millisecond)
+				Eventually(sess, settings.MaxEventuallyTimeout).Should(Say("Scaling processes... but first,"))
 				Eventually(sess, settings.MaxEventuallyTimeout).Should(Say(`done in \d+s`))
 				Eventually(sess).Should(Say("=== %s Processes", app.Name))
-				Eventually(sess).Should(Say("--- %s:", "web"))
+				Eventually(sess).Should(Say(`(--- [\w]+)`))
 				Eventually(sess).Should(Say(`(%s-[\w-]+) up \(v\d+\)`, app.Name))
-
+				Eventually(sess).Should(Say(`(--- [\w]+)`))
+				Eventually(sess).Should(Say(`(%s-[\w-]+) up \(v\d+\)`, app.Name))
+				Eventually(sess, settings.MaxEventuallyTimeout).Should(Exit(0))
 			})
 
 		})
